cdd63a624518f02647c58bec54d9b424134da301
kusidora <toradora860@gmail.com>
2025-02-23 11:32:33 +0900

    色の調整

--- src/components/ui/Side.tsx
+++ src/components/ui/Side.tsx
@@ -14,7 +14,7 @@ const Side = () => {
             <li>
               <a
                 href="/todo"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className="flex items-center p-2 !text-gray-900 rounded-lg dark:!text-white hover:bg-gray-300 dark:hover:bg-teal-800 group"
               >
                 <FaTasks />
                 <span className="ms-3">Todo</span>
@@ -23,7 +23,7 @@ const Side = () => {
             <li>
               <a
                 href="/timer"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className="flex items-center p-2 !text-gray-900 rounded-lg dark:!text-white hover:bg-gray-300 dark:hover:bg-teal-800 group"
               >
                 <IoIosTimer />
                 <span className="flex-1 ms-3 whitespace-nowrap">Timer</span>
